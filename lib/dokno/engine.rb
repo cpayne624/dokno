35958b3ce364e53bb04abee4c456bbc0239f6465
Courtney Payne <cpayne624@gmail.com>
2020-11-29 15:54:18 -0500

    Re-organize specs and add Simplecov

--- lib/dokno/engine.rb
+++ lib/dokno/engine.rb
@@ -4,9 +4,15 @@ module Dokno
   class Engine < ::Rails::Engine
     isolate_namespace Dokno
 
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     initializer :append_migrations do |app|
-      config.paths["db/migrate"].expanded.each do |expanded_path|
-        app.config.paths["db/migrate"] << expanded_path
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
       end
     end
 
---
+++ spec/models/dokno/category_spec.rb
@@ -0,0 +1,35 @@
+module Dokno
+  describe Category do
+    context 'validation' do
+      it 'requires a name' do
+        category = Category.new
+
+        expect(category.validate).to be false
+        expect(category.errors[:name]).to include 'can\'t be blank'
+        expect(Category.new(name: 'dummy').valid?).to be true
+      end
+
+      it 'must have a unique name' do
+        categories = [Category.create(name: 'dummy'), Category.create(name: 'dummy 2')]
+        categories.first.name = categories.last.name
+
+        expect(categories.first.validate).to be false
+        expect(categories.first.errors[:name]).to include 'has already been taken'
+
+        categories.first.name = 'dummy 3'
+        expect(categories.first.validate).to be true
+      end
+
+      it 'can not have its parent set to self' do
+        categories = [Category.create(name: 'dummy'), Category.create(name: 'dummy 2')]
+        categories.first.category_id = categories.first.id
+
+        expect(categories.first.validate).to be false
+        expect(categories.first.errors[:category_id]).to include 'can\'t set parent to self'
+
+        categories.first.category_id = categories.last.id
+        expect(categories.first.validate).to be true
+      end
+    end
+  end
+end
