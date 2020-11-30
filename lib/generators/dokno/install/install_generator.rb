15eec4cde34b2434e04f4bb40e89829ca099b6f8
Courtney Payne <cpayne624@gmail.com>
2020-11-30 13:34:13 -0500

    Add article template

--- lib/generators/dokno/install/install_generator.rb
+++ lib/generators/dokno/install/install_generator.rb
@@ -3,9 +3,10 @@ module Dokno
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../../templates', __FILE__)
 
-      desc 'Copys the Dokno template initializer to the host application'
+      desc 'Copies the Dokno configuration templates to the host app'
       def copy_initializer
         template 'config/initializers/dokno.rb', 'config/initializers/dokno.rb'
+        template 'config/dokno_template.md', 'config/dokno_template.md'
       end
     end
   end
---
+++ lib/generators/dokno/templates/config/dokno_template.md
@@ -0,0 +1,5 @@
+This is your article template. It's helpful for adding consistency to your app's knowledgebase articles.
+
+Articles support <strong>basic HTML</strong> and **markdown**.
+
+You can modify this template in `/config/dokno_template.md`
